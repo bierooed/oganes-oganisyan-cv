556342c2524610a72d05443f9f6c63451d66f82a
bierooed <hov.hovanes13@gmail.com>
2023-08-04 09:04:57 +0400

    Some style changes

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -7,7 +7,7 @@ export default function HomePage() {
     <div className="flex flex-col items-center">
       <img className="w-28" src={avatar} />
       <div className="text-center mt-4">
-        <h2 className="text-xl">My name is Oganes! 🖖🏻</h2>
+        <h2 className="text-xl">🖖🏻</h2>
         <p className="text-md text-gray-500">
           I&apos;m a Junior Engineer with a passion for design and project
           creation
